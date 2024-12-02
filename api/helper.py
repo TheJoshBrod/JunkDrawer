d1c34e998e37333e36b7edc8816277e96915aa78
joshbrod <joshbrod@umich.edu>
2024-12-01 22:36:30 -0500

    fixed math issue with file of size 0

--- api/helper.py
+++ api/helper.py
@@ -85,8 +85,9 @@ def create_child(result: list) -> dict:
 
     file_sizes = ["B", "KB", "MB", "GB", "TB"]
     size = result[2]
-    if is_file and size != "0":
+    if is_file and size != 0:
         int_size = int(size)
+        print(int_size)
         log_size = int(math.log(int_size, 1000))
         size = str(round(int_size/(1000 ** (log_size)), 2)) + " " + file_sizes[log_size]
     else:
